3abb7000cc9883e758fdb9686beed27cd0835ea1
brodokk <brodokk@brodokk.space>
2023-01-06 01:48:02 +0100

    Clean srt return function

--- jellyfin2txt/subtitle.py
+++ jellyfin2txt/subtitle.py
@@ -157,14 +157,14 @@ class Subtitle:
 
     @staticmethod
     def subtitle(item_id, subtitle_name):
-        item_id='24b6d39b4779259fda28d856e9479b66' # psg quick
-        #item_id='ce096db83d3454055cf0b5315284c947' # pgs slow: a lot
-        #item_id='32b81e45fa24eef35b6305bbd5c2329a'
-        #item_id='c792916f205221d1d84c73baad5e9814'
-        data = client.jellyfin.get_play_info(
-            item_id=item_id,
-            profile=self.profile
-        )
+        try:
+            data = client.jellyfin.get_play_info(
+                item_id=item_id,
+                profile=Subtitle.profile
+            )
+        except jellyfin_apiclient_python_HTTPException:
+            return "Item not existing on Jellyfin", 404
+
         name = data['MediaSources'][0]['Path'].split('/')[-1]
 
         subtitles_file_supported = ['subrip', 'ass', 'PGSSUB', 'mov_text']
@@ -174,76 +174,25 @@ class Subtitle:
             format_supported = False
             if media['Type'] == 'Subtitle':
                 if media['DisplayTitle'] != subtitle_name:
-                    print('incorrect name')
+                    continue
                 if media['Codec'] not in subtitles_file_supported:
-                    print('incorrect codec')
+                    continue
                 codec = media["Codec"]
                 final_filename = Path(f"{name.replace('/', '')}.{media['DisplayTitle'].replace('/', '')}.srt")
                 if media['IsExternal'] or media['IsTextSubtitleStream'] or media['SupportsExternalStream']:
                     url = f"{app.config['SERVER_URL']}{media['DeliveryUrl']}"
-                    if codec not in neos_subtitles_file_supported:
-                        if codec == 'ass':
-                            with tempfile.NamedTemporaryFile() as sub_temp_file:
-                                urllib.request.urlretrieve(url, sub_temp_file.name)
-                                sub = pyasstosrtSubtitle(sub_temp_file.name)
-                                tmp_filename = sub_temp_file.name.split('/')[-1]
-                                sub.export(output_dir=subtitles_output_folder)
-                                os.replace(f"{subtitles_output_folder/tmp_filename}.srt", f"{subtitles_output_folder/final_filename}")
-                        elif codec == 'mov_text':
-                            with tempfile.NamedTemporaryFile() as sub_temp_file:
-                                urllib.request.urlretrieve(url, sub_temp_file.name)
-                                clean_sub(sub_temp_file.name, final_filename)
-                        else:
-                            format_supported = False
                     format_supported = True
-                elif  media['Codec'] == 'PGSSUB':
-                    print('go pgsrip')
-                    with tempfile.TemporaryDirectory(dir='tmp') as sub_temp_dir:
-                        #urllib.request.urlretrieve(url, sub_temp_dir)
-                        #clean_sub(sub_temp_file.name, final_filename)
-                        sub_temp_file = Subtitle.download(item_id, Path(sub_temp_dir) / Path(name))
-                        from sh import pgsrip
-                        from pgsrip import pgsrip, Mkv, Options
-                        from babelfish import Language
-
-                        media_file = Mkv(sub_temp_file)
-                        options = Options(languages={Language('eng')}, overwrite=True, one_per_lang=False)
-                        pgsrip.rip(media_file, options)
-                        from sh import ls
-
-                        print(ls('-alh', sub_temp_dir))
-                        print(sub_temp_file)
-                        #print(sub_temp_file)
-                        #import time
-                        #time.sleep(10000)
-                        #pgsrip('-a', sub_temp_file)
-
-                        for entry in Path(sub_temp_dir).iterdir():
-                            if entry.is_file() and entry.suffix == '.srt':
-                                print(entry.stem.replace('/', ''))
-                                final_filename = Path(f"{entry.stem.replace('/', '')}.srt")
-                                clean_sub(sub_temp_file, f"{subtitles_output_folder/final_filename}")
-                                url = 'uwu'
-                    # for extract pgsrip use https://github.com/ratoaq2/pgsrip
                 else:
                     format_supported = False           
                 
                 if format_supported:
-                    print(media)
-                    print(url)
-                    print(subtitles)
-                    print(media['DisplayTitle'])
                     return url
                 else:
                     print(f"Warning format {media['DisplayTitle']} {codec} not suported for item id {item_id}")
                     if media['IsExternal'] or media['IsTextSubtitleStream'] or media['SupportsExternalStream']:
                         print('This format seems to be easly convertable in srt')
-                    url = f"{app.config['SERVER_URL']}{media['DeliveryUrl']}"
-                    print(url)
-
-            print("err")
     
-        return "Invalid id?"
+        return "Error while returning the srt", 500
 
     @staticmethod
     def subtitles_old(item_id):
