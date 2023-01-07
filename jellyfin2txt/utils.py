7a2bd54e71b79de80bec82eaaa59c19cff6a8b29
brodokk <brodokk@brodokk.space>
2023-01-07 13:47:11 +0100

    Add libs

--- jellyfin2txt/utils.py
+++ jellyfin2txt/utils.py
@@ -13,3 +13,10 @@ def _read_keyfile():
     for key in keys_json:
         keys.append(Key(**key), 'id')
     return keys
+
+def sizeof_fmt(num, suffix="B"):
+    for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
+        if abs(num) < 1024.0:
+            return f"{num:3.1f}{unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f}Yi{suffix}"
